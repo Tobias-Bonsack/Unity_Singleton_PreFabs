5c4233f1d1efcca43853568f543ce8b0a6eb538d
Tobias-Bonsack <tobias.bonsack@gmail.com>
2021-10-01 12:42:13 +0200

    implement third person controller

--- Assets/Third-Person Controller/Scripts/FreeLookAddOn.cs
+++ Assets/Third-Person Controller/Scripts/FreeLookAddOn.cs
@@ -9,8 +9,6 @@ namespace ThirdPersonController
     [RequireComponent(typeof(CinemachineFreeLook))]
     public class FreeLookAddOn : MonoBehaviour
     {
-        [Range(0f, 10f)] [SerializeField] float _lookSpeed = 1f;
-        [SerializeField] bool _invertY = false;
         private CinemachineFreeLook _freeLookComponent;
 
         public void Start()
@@ -23,14 +21,13 @@ namespace ThirdPersonController
         {
             //Normalize the vector to have an uniform vector in whichever form it came from (I.E Gamepad, mouse, etc)
             Vector2 mouseMovement = context.ReadValue<Vector2>().normalized;
-            mouseMovement.y = _invertY ? -mouseMovement.y : mouseMovement.y;
 
             // This is because X axis is only contains between -180 and 180 instead of 0 and 1 like the Y axis
             mouseMovement.x = mouseMovement.x * 180f;
 
             //Ajust axis values using look speed and Time.deltaTime so the look doesn't go faster if there is more FPS
-            _freeLookComponent.m_XAxis.Value += mouseMovement.x * _lookSpeed * Time.deltaTime;
-            _freeLookComponent.m_YAxis.Value += mouseMovement.y * _lookSpeed * Time.deltaTime;
+            _freeLookComponent.m_XAxis.Value += _freeLookComponent.m_XAxis.m_MaxSpeed * mouseMovement.x * Time.deltaTime;
+            _freeLookComponent.m_YAxis.Value += _freeLookComponent.m_YAxis.m_MaxSpeed * mouseMovement.y * Time.deltaTime;
         }
     }
 }
--- Assets/Third-Person Controller/Scripts/PlayerManager.cs
+++ Assets/Third-Person Controller/Scripts/PlayerManager.cs
@@ -1,21 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace ThirdPersonController
 {
-public class PlayerManager : MonoBehaviour
+    public class PlayerManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField] GameObject _player;
+        private CharacterController _controller;
+        [SerializeField] Transform _camera;
+
+        [Header("Move")]
+        [SerializeField] float _speed;
+        [SerializeField] float _turnSpeed;
+        private float _turnSmoothVelocity;
+        private bool _isMoving = false;
+        private Vector3 _direction;
+
+        private void Awake()
         {
+            _controller = _player.GetComponent<CharacterController>();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            _isMoving = !context.canceled;
+            if (_isMoving)
+            {
+                //Normalize the vector to have an uniform vector in whichever form it came from (I.E Gamepad, mouse, etc)
+                Vector2 moveDirection = context.ReadValue<Vector2>().normalized;
+                _direction = new Vector3(moveDirection.x, 0f, moveDirection.y);
+            }
 
         }
 
-        // Update is called once per frame
-        void Update()
+        private void FixedUpdate()
         {
 
+            if (_isMoving)
+            {
+                float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSpeed);
+                _player.transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                _controller.Move(moveDir * _speed * Time.deltaTime);
+            }
         }
     }
 }
