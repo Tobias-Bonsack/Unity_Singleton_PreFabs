1b9fa7ce6dd098c16c662722006c227bc7eb19b3
Tobias-Bonsack <tobias.bonsack@gmail.com>
2021-10-01 11:30:05 +0200

    implement solution for api between input system and cinemachine

--- Assets/Third-Person Controller/Scripts/FreeLookAddOn.cs
+++ Assets/Third-Person Controller/Scripts/FreeLookAddOn.cs
@@ -1,21 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace ThirdPersonController
 {
+    [RequireComponent(typeof(CinemachineFreeLook))]
     public class FreeLookAddOn : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        [Range(0f, 10f)] [SerializeField] float _lookSpeed = 1f;
+        [SerializeField] bool _invertY = false;
+        private CinemachineFreeLook _freeLookComponent;
 
+        public void Start()
+        {
+            _freeLookComponent = GetComponent<CinemachineFreeLook>();
         }
 
-        // Update is called once per frame
-        void Update()
+        // Update the look movement each time the event is trigger
+        public void OnLook(InputAction.CallbackContext context)
         {
+            //Normalize the vector to have an uniform vector in whichever form it came from (I.E Gamepad, mouse, etc)
+            Vector2 mouseMovement = context.ReadValue<Vector2>().normalized;
+            mouseMovement.y = _invertY ? -mouseMovement.y : mouseMovement.y;
 
+            // This is because X axis is only contains between -180 and 180 instead of 0 and 1 like the Y axis
+            mouseMovement.x = mouseMovement.x * 180f;
+
+            //Ajust axis values using look speed and Time.deltaTime so the look doesn't go faster if there is more FPS
+            _freeLookComponent.m_XAxis.Value += mouseMovement.x * _lookSpeed * Time.deltaTime;
+            _freeLookComponent.m_YAxis.Value += mouseMovement.y * _lookSpeed * Time.deltaTime;
         }
     }
 }
